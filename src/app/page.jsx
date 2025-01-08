3d8f315ad3900131d6ac5928958d39202f460cac
irdepla <rixsiboyevislombek@gmail.com>
2025-01-08 20:55:07 +0500

    dd

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,25 +1,19 @@
 import Image from "next/image";
-import Card from "./components/Card";
-
+import Card from "./components/Card/index.jsx";
 
 export default async function Home() {
+  const PRODUCTS_GET_URL = "http://localhost:5000/products";
 
-
-  const PRODUCTS_GET_URL = "http://localhost:5000/products"
-
-
-    const products = (await (await fetch(PRODUCTS_GET_URL)).json()).data || [];
-    console.log(products);
-    
-  
+  const products = (await (await fetch(PRODUCTS_GET_URL)).json()).data || [];
+  console.log(products);
 
   return (
     <>
-    <div className="grid grid-cols-3 gap-3">
-    {
-      products.map((product) => (<Card key={product.id} product={product} />))
-    }
-    </div>
+      <div className="grid grid-cols-3 gap-3">
+        {products.map((product) => (
+          <Card key={product.id} product={product} />
+        ))}
+      </div>
     </>
   );
 }
